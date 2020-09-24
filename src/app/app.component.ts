262e547e36a419bc34d012b66ee9e7b06df707e7
Mason Adrales <masonadrales@Masons-MacBook-Pro-4.local>
2020-09-24 11:41:06 -0500

    Added input, stored to array, and output array to display

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo-app';
+
+  todoArray = [];
+
+  addTodo(value){
+    if(value != ""){
+      this.todoArray.push(value);
+      console.log(this.todoArray);
+    }else {
+      alert("Todo required!!");
+    }
+  }
+}
